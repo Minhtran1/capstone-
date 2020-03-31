b997d896e27bc011595d3f7d527c5ded79346a23
Minh Tran <minhbtran@yahoo.com>
2020-03-30 23:18:29 -0500

    small change

--- store /Gallery.js
+++ store /Gallery.js
@@ -5,21 +5,40 @@ export default {
     {
       url:
           "https://raw.githubusercontent.com/Minhtran1/capstone-/master/Pictures/Singapore.JPG"
-          location: "Singapore"
+        title: "Singapore"
     },
     {
-
-      url:https://github.com/Minhtran1/capstone-/blob/master/Pictures/vietnam.jpg?raw=true"
-        location: "Vietnam"
-    }
-
-
-
-
-
-
-
-
+      url:https://github.com/Minhtran1/capstone-/blob/master/Pictures/Vietnam.jpg?raw=true"
+        title: "Vietnam"
+    },
+    {
+      url:
+        "https://github.com/Minhtran1/capstone-/blob/master/Pictures/Malaka.JPG?raw=true",
+        title: Malaysia
+    },
+    {
+      url:
+        "https://github.com/Minhtran1/capstone-/blob/master/Pictures/Tree.JPG?raw=true",
+    },
+    {
+    url:
+    "https://github.com/Minhtran1/capstone-/blob/master/Pictures/Light.JPG?raw=true",
+        title: "Light show"
+    },
+    {
+    url:
+    "https://github.com/Minhtran1/capstone-/blob/master/Pictures/Bana.JPG?raw=true",
+        title: "Bana Hills"
+    },
+    {
+    url:
+    "https://github.com/Minhtran1/capstone-/blob/master/Pictures/Malaysia.JPG?raw=true",
+        title: "Patronas Towers"
+    },
+    {
+    url:
+    "https://github.com/Minhtran1/capstone-/blob/master/Pictures/Hand.JPG?raw=true",
+        title: "Golden Bridge"
     }
   ]
 };
