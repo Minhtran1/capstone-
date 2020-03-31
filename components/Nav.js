c4d14a9bebda24fb972613082863b4c390e83abb
Minh Tran <minhbtran@yahoo.com>
2020-03-31 01:03:23 -0500

    changes

--- components/Nav.js
+++ components/Nav.js
@@ -1,11 +1,19 @@
-export default () => `
+export default links => `
 <nav>
-  <li class="active"><a href="#">Home</a></li>
-  <li><a href="#">Gallery</a></li>
+  <li class="fas fa-bars"></i>
+  <ul class ="hidden--mobile nav-link">
+  <a href="#">Home</a></li>
+  ${links.reduce(
+    (html, link) =>
+      html +
+      `li><a href="#">Gallery</a></li>
       <li><a href="#">Cities</a></li>
       <li><a href="#">About</a></li>
       <li><a href="#">Contact</a></li>
       <li><a href="#">More</a></li>
+      link.title
+  }" data-navigo>${link.text}</a></li`,
+    ``
+  )}
   </ul>
-  </nav>
-  `;
+  </nav> `;
