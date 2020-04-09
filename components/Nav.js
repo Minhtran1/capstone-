8614c5d58e837c2ef85c2791450ad790155769eb
Vanessa Kratohvil <vanessakratohvil@gmail.com>
2020-04-08 21:00:14 -0500

    Remove unnecessary code comments

--- components/Nav.js
+++ components/Nav.js
@@ -12,17 +12,3 @@ export default links => `
     .join("")}
   </ul>
   </nav> `;
-
-//   <nav>
-//   <div class="logo">
-//       <h4>Marco Polo</h4>
-//   </div>
-//   <ul class="nav-links" id="nav-links">
-//       ${links.map(link => `<li><a href="#" aria-label=${link.text}>${link.title}</a></li>`).join('')}
-//   </ul>
-//   <div class="burger">
-//       <div class="line1"></div>
-//       <div class="line2"></div>
-//       <div class="line3"></div>
-//   </div>
-// </navnav>`
